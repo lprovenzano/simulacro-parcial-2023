5fd79b7df70051ede8fbc8531f1289927aca7416
Luca Eros Provenzano <luca.provenzano@mercadolibre.com>
2023-04-27 20:35:29 -0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'bienvenido', component: BienvenidoComponent },
+  { path: 'busqueda', component: BusquedaComponent },
+  { path: '',   redirectTo: '/bienvenido', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
+import { PeliculaAltaComponent } from './components/pelicula-alta/pelicula-alta.component';
+import { ActorAltaComponent } from './components/actor-alta/actor-alta.component';
+import { ActorListadoComponent } from './components/actor-listado/actor-listado.component';
+import { PeliculaListadoComponent } from './components/pelicula-listado/pelicula-listado.component';
+import { TablaPeliculaComponent } from './components/tabla-pelicula/tabla-pelicula.component';
+import { DetallePeliculaComponent } from './components/detalle-pelicula/detalle-pelicula.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BienvenidoComponent,
+    BusquedaComponent,
+    PeliculaAltaComponent,
+    ActorAltaComponent,
+    ActorListadoComponent,
+    PeliculaListadoComponent,
+    TablaPeliculaComponent,
+    DetallePeliculaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
