5ad4ba233b26970967071c54e76bbec679477296
Luca Eros Provenzano <luca.provenzano@mercadolibre.com>
2023-05-14 17:34:50 -0300

    actor alta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { PeliculaAltaComponent } from './components/pelicula-alta/pelicula-alta.component';
 import { ActorAltaComponent } from './components/actor-alta/actor-alta.component';
 import { BusquedaComponent } from './components/busqueda/busqueda.component';
 import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'bienvenido', component: BienvenidoComponent },
   { path: 'busqueda', component: BusquedaComponent },
   { path: 'actor-alta', component: ActorAltaComponent },
+  { path: 'pelicula-alta', component: PeliculaAltaComponent },
   { path: '',   redirectTo: '/bienvenido', pathMatch: 'full' },
 ];
 
